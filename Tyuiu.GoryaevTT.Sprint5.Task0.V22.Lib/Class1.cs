1c980728ede109cefe48d5be0f8f517d83105ecd
GoryaevTagir <Lobstera134@gmail.com>
2025-11-08 02:32:31 +0500

    Task0

--- Tyuiu.GoryaevTT.Sprint5.Task0.V22.Lib/Class1.cs
+++ Tyuiu.GoryaevTT.Sprint5.Task0.V22.Lib/Class1.cs
@@ -7,7 +7,7 @@ namespace Tyuiu.GoryaevTT.Sprint5.Task0.V22.Lib
         public string SaveToFileTextData(int x)
         {
             string way = Path.Combine(Path.GetTempPath(), "OutPutFileTask0.txt");
-            double res = Math.Round(Math.Pow(1 - x, 2) / (-3 * x), 2);
+            double res = Math.Round(Math.Pow(1 - x, 2) / (-3 * x), 3);
             File.WriteAllText(way, Convert.ToString(res));
             return way;
         }
