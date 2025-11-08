55aa6e8f9637ce475cfe4096bf5b1c9d74174ede
GoryaevTagir <Lobstera134@gmail.com>
2025-11-09 03:53:46 +0500

    Task4

--- Tyuiu.GoryaevTT.Sprint5.Task4.V14.Lib/Class1.cs
+++ Tyuiu.GoryaevTT.Sprint5.Task4.V14.Lib/Class1.cs
@@ -5,6 +5,13 @@ namespace Tyuiu.GoryaevTT.Sprint5.Task4.V14.Lib
     {
         public double LoadFromDataFile(string path)
         {
+            foreach (string el in File.ReadAllLines(path))
+            {
+                if (el == ".")
+                {
+                    File.ReadAllText(path).Replace(".", ",");
+                }
+            }
             double x = Convert.ToDouble(File.ReadAllText(path));
             double res = Math.Round(Math.Sin(Math.Pow(x, 3)) + 2 / x, 3);
             return res;
