bd159b731c7f9c683af5eb654c87ef3849f6191e
GoryaevTagir <Lobstera134@gmail.com>
2025-11-09 01:49:45 +0500

    Task2

--- Tyuiu.GoryaevTT.Sprint5.Task2.V30.Lib/Class1.cs
+++ Tyuiu.GoryaevTT.Sprint5.Task2.V30.Lib/Class1.cs
@@ -13,9 +13,9 @@ namespace Tyuiu.GoryaevTT.Sprint5.Task2.V30.Lib
             {
                 File.Delete(path);
             }
-            for (int i = 0; i <= matrix.GetLength(0); i++)
+            for (int i = 0; i < matrix.GetLength(0); i++)
             {
-                for (int j = 0; j <= matrix.GetLength(1); j++)
+                for (int j = 0; j < matrix.GetLength(1); j++)
                 {
                     if (matrix[i, j] > 0)
                     {
@@ -28,11 +28,11 @@ namespace Tyuiu.GoryaevTT.Sprint5.Task2.V30.Lib
                 }
             }
             string xxx = "";
-            for (int i = 0; i <= matrix.GetLength(0); i++)
+            for (int i = 0; i < matrix.GetLength(0); i++)
             {
-                for (int j = 0;j <= matrix.GetLength(1); j++)
+                for (int j = 0;j < matrix.GetLength(1); j++)
                 {
-                    if (j != matrix.GetLength(0))
+                    if (j != matrix.GetLength(0) - 1)
                     {
                         xxx += matrix[i, j] + ";";
                     }
@@ -41,7 +41,7 @@ namespace Tyuiu.GoryaevTT.Sprint5.Task2.V30.Lib
                         xxx += matrix[i, j];
                     }
                 }
-                if (i != matrix.GetLength(0))
+                if (i != matrix.GetLength(0) - 1)
                 {
                     File.AppendAllText(path, xxx + Environment.NewLine);
                 }
